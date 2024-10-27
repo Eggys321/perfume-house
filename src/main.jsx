c2a5e1c2416fc5baa14ee39289e76f83e047344e
eguono imonieroh <imonieroheguono@gmail.com>
2024-10-27 16:18:52 +0100

    fixes:contextApi

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,25 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
-import App from "./App.jsx";
+// import App from "./App.jsx";
 import "./index.css";
 import { CartProvider } from "./context/CartContext.jsx";
-
+import { lazy, Suspense } from "react";
+import Spinner from "react-bootstrap/Spinner";
+const App = lazy(() => import("./App"));
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <CartProvider>
-      <App />
+    <Suspense
+        fallback={
+          <div className="d-flex justify-content-center align-items-center vh-100">
+            <Spinner animation="grow" variant="secondary" />
+            <h5 className="pt-2"> Loading...</h5>
+          </div>
+        }
+      >
+        <App />
+      </Suspense>
     </CartProvider>
   </StrictMode>
 );
